a719987c8e4c39dc7eab0b583bdec2a4e1cf1502
Chelsea Neal <chelseann3@gmail.com>
2019-06-20 19:25:32 -0400

    Add sponsor logos and animation

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 toggle between hiding and showing the dropdown content */
 
   window.onload = function() {
-    document.querySelector(".menu-button").addEventListener("click", function() {
+    document.querySelector("#hamburger-menu").addEventListener("click", function() {
       document.getElementById("myDropdown").classList.toggle("show");  
     });
   };
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.menu-button')) {
+    if (!event.target.matches('#hamburger-menu')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
