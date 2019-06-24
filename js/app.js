63405642968e3bc5efe79f775eb258cf7098213e
Chelsea Neal <chelseann3@gmail.com>
2019-06-24 17:10:05 -0400

    Create jquery dropdown menu

--- js/app.js
+++ js/app.js
@@ -1,22 +1,9 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+// Hamburger menu is hidden //
+$('.dropdown-content').hide();
 
-  window.onload = function() {
-    document.querySelector("#hamburger-menu").addEventListener("click", function() {
-      document.getElementById("myDropdown").classList.toggle("show");  
-    });
-  };
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('#hamburger-menu')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+// When user clicks on the hamburger menu, dropdown content shows, icon changes to exit icon //
+
+$('#menu-icons').click(function () {
+  $('.dropdown-content').toggle("slow");
+  $(this).find('img').toggle();
+});
